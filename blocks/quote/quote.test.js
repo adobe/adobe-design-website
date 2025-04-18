507b55b9beee1e5119c1fbf7c165a77638f463c8
SaaSha Pina <saasha@heysparkbox.com>
2025-04-18 10:40:32 -0400

    feat: add blockquote and tests

--- blocks/quote/quote.test.js
+++ blocks/quote/quote.test.js
@@ -3,9 +3,41 @@ describe('Quote Block', () => {
     await page.goto(`${global.BASE_URL}pattern-library/`);
   });
 
-  it('should render the quote wrapper', async () => {
-    await page.waitForSelector('.quote-wrapper');
-    const quoteWrapper = await page.$('.quote-wrapper');
-    expect(quoteWrapper).toExist();
+  it('should render the blockquote element', async () => {
+    await page.waitForSelector('.quote blockquote');
+    const blockquote = await page.$('.quote blockquote');
+    expect(blockquote).toExist();
+  });
+
+  it('should render the quote text', async () => {
+    await page.waitForSelector('.quote-quotation');
+    const quoteText = await page.$('.quote-quotation');
+    expect(quoteText).toExist();
+  });
+
+  it('should render the attribution when present', async () => {
+    await page.waitForSelector('.quote-attribution');
+    const attribution = await page.$('.quote-attribution');
+    expect(attribution).toExist();
+  });
+
+  it('should render the attribution with cite element', async () => {
+    await page.waitForSelector('.quote-attribution cite');
+    const cite = await page.$('.quote-attribution cite');
+    expect(cite).toExist();
+  });
+
+  it('should render the attribution text in a paragraph', async () => {
+    await page.waitForSelector('.quote-attribution cite p');
+    const attributionText = await page.$('.quote-attribution cite p');
+    expect(attributionText).toExist();
+  });
+
+  it('should render attribution link when URL is provided', async () => {
+    await page.waitForSelector('.quote-attribution cite p a');
+    const attributionLink = await page.$('.quote-attribution cite p a');
+    if (attributionLink) {
+      expect(attributionLink).toExist();
+    }
   });
 });
