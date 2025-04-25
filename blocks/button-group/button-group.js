2efb9f269fcb9eab759939292b51aa3fb22f41bf
Osmond Arnesto <53844657+arnest00@users.noreply.github.com>
2025-04-25 10:32:48 -0700

    feat: button styles and button-group (#54)

---
+++ blocks/button-group/button-group.js
@@ -0,0 +1,21 @@
+export default async function decorate(block) {
+  const buttons = [...block.children].map((row) => ({
+    anchorNode: row.children[0].firstChild.firstChild,
+    buttonStyle: row.children[1].firstChild,
+    altText: row.children[2].firstChild
+  }));
+
+  const buttonGroupContainer = document.createElement('div');
+  buttonGroupContainer.classList = 'button-group';
+
+  buttons.forEach(button => {
+    button.anchorNode.classList = 'button';
+    button.buttonStyle
+      ? button.anchorNode.classList.add(`button--${button.buttonStyle.innerText}`)
+      : button.anchorNode.classList.add(`button--primary`);
+    if (button.altText) button.anchorNode.title = button.altText.innerText;
+    buttonGroupContainer.append(button.anchorNode);
+  });
+
+  block.replaceWith(buttonGroupContainer);
+}
