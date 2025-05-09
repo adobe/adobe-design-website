7fdca6ba807f2eb24ba8816dac1bb5cbc2489641
Josh Winn <965114+jawinn@users.noreply.github.com>
2025-05-09 16:09:43 -0400

    refactor: shared utility class for quote and lede
    Refactor lede and quote blocks to use a shared utility class, as they
    use the same set of responsive font sizes. With two utility classes, the
    lede CSS file was removed as it was no longer necessary.

--- blocks/lede/lede.js
+++ blocks/lede/lede.js
@@ -5,8 +5,9 @@
  */
 export default function decorate(block) {
     // Row of content from the block; typically one or more paragraphs.
-    // Converts HTMLCollection to array of nodes so it's usable by replaceChildren.
+    // Converts HTMLCollection (live collection) to a static array of nodes, so it's usable by replaceChildren.
     const contentArray = Array.from(block?.children?.[0]?.children?.[0]?.children || []);
+    contentArray.forEach(el => el.classList.add('util-heading-quote', 'util-bold-font-weight'));
 
     // Replace starting markup within our parent block div, to remove excess div wrappers.
     block.replaceChildren(...contentArray);
