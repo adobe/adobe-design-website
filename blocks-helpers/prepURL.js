22a7e7d8ebea7f96abff67d0a16552f54e938d53
Catherine Meade <cat@heysparkbox.com>
2025-05-13 10:45:57 -0400

    docs: use JSDocs syntax for helpers comments

--- blocks-helpers/prepURL.js
+++ blocks-helpers/prepURL.js
@@ -1,6 +1,11 @@
-// takes a URL string and prepares it to be used as a URL slug
-// by making it lower case, removing non-alphanumeric characters,
-// and replacing spaces with dashes
+/**
+ * Takes a URL string and prepares it to be used as a URL slug
+ * by making it lower case, removing non-alphanumeric characters,
+ * and replacing spaces with dashes
+ * @function
+ * @param {string} string - The piece of text to be translated
+ * @return {string} in slug format, e.g. url-slug
+ */
 
 export const prepURL = (string) => {
   const slug = string
--- blocks-helpers/skipLinks.js
+++ blocks-helpers/skipLinks.js
@@ -1,3 +1,9 @@
+/**
+ * Builds a bypass block that links to the page's main content element.
+ * @function
+ * @return {string} a static HTML anchor node
+ */
+
 export const buildSkipLink = () => {
   const skipToContentLink = document.createElement('a');
   skipToContentLink.href = "#main-content";
