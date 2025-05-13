733ebca3dbe0b17ad9bad1c264c1407eaacde004
Catherine Meade <catheraaine@gmail.com>
2025-05-13 11:34:55 -0400

    feat: section header (#71)

--- blocks/button-group/button-group.js
+++ blocks/button-group/button-group.js
@@ -1,8 +1,9 @@
 export default async function decorate(block) {
   const buttons = [...block.children].map((row) => ({
-    anchorNode: row.children[0].firstChild.firstChild,
-    buttonStyle: row.children[1].firstChild,
-    altText: row.children[2].firstChild
+    anchorNode: row.children[0]?.firstChild.firstChild,
+    buttonStyle: row.children[1]?.firstChild,
+    altText: row.children[2]?.firstChild,
+    hideButtonOnLargeScreens: row.children[3]?.firstChild,
   }));
 
   const buttonGroupContainer = document.createElement('div');
@@ -13,7 +14,11 @@ export default async function decorate(block) {
     button.buttonStyle
       ? button.anchorNode.classList.add(`button--${button.buttonStyle.innerText}`)
       : button.anchorNode.classList.add(`button--primary`);
+
     if (button.altText) button.anchorNode.title = button.altText.innerText;
+
+    if (button.hideButtonOnLargeScreens) button.anchorNode.classList.add("button--hide-at-large");
+
     buttonGroupContainer.append(button.anchorNode);
   });
 
