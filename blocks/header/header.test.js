af5666c3ee77ce505b47848628b6ba337e828583
Catherine Meade <cat@heysparkbox.com>
2025-05-17 14:50:09 -0400

    test: add a few more test cases

--- blocks/header/header.test.js
+++ blocks/header/header.test.js
@@ -1,5 +1,6 @@
-describe('Header Block', () => {
+describe('Header Navigation Block', () => {
   beforeAll(async () => {
+    await page.setViewport({ width: 700, height: 1000 });
     await page.goto(`${global.BASE_URL}`);
     await page.waitForSelector('.nav');
   });
@@ -7,6 +8,9 @@ describe('Header Block', () => {
   it('should render the nav', async () => {
     const nav = await page.$('.nav');
     expect(nav).toExist();
+
+    const navWrapper = await page.$('.nav-wrapper');
+    expect(navWrapper).toExist();
   });
 
   it('should render the nav contents', async () => {
@@ -18,5 +22,29 @@ describe('Header Block', () => {
 
     const navTools = await page.$('.nav__toolbar');
     expect(navTools).toExist();
+  });
+
+  it('should render a menu button and panel', async () => {
+    const menuPanel = await page.$('.nav__menu');
+    expect(menuPanel).toExist();
+
+    const menuButton = await page.$('.nav__menu-button');
+    expect(menuButton).toExist();
+  });
+
+  describe('On large screens', () => {
+    beforeAll(async () => {
+      await page.setViewport({ width: 1500, height: 1000 });
+      await page.goto(`${global.BASE_URL}`);
+      await page.waitForSelector('.nav');
+    });
+
+    it('should not render a menu button or panel', async () => {
+      const menuPanel = await page.$('.nav__menu');
+      expect(menuPanel).toBeNull();
+
+      const menuButton = await page.$('.nav__menu-button');
+      expect(menuButton).toBeNull();
+    });
   });
 });
