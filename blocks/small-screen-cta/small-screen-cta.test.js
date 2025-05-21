8fb667386ced2822160e43bc2ed6a72fcc681980
Catherine Meade <cat@heysparkbox.com>
2025-05-20 21:52:00 -0400

    feat: small screen cta

---
+++ blocks/small-screen-cta/small-screen-cta.test.js
@@ -0,0 +1,25 @@
+describe('Small Screen CTA Block', () => {
+  beforeAll(async () => {
+    await page.setViewport({ width: 350, height: 1000 });
+    await page.goto(`${global.BASE_URL}pattern-library/`);
+    await page.$('.call-to-action');
+  });
+
+  it('On small screens should render the cta block', async () => {
+    const cta = page.$('.call-to-action');
+    expect(cta).toExist();
+  });
+
+  describe("On large screens", () => {
+    beforeAll(async () => {
+      await page.setViewport({ width: 1500, height: 1000 });
+      await page.goto(`${global.BASE_URL}`);
+      await page.$('#main-content');
+    });
+
+    it("should not render the cta block", async () => {
+      const cta = await page.waitForSelector('.call-to-action', { hidden: true, timeout: 3000 });
+      expect(cta).toBeNull();
+    });
+  });
+});
