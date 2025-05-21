9921cdde8cddad625a90c7c7306ac38a190bef25
Josh Winn <965114+jawinn@users.noreply.github.com>
2025-05-21 12:28:49 -0400

    refactor: filter group - reorganize into separate files
    Separate out functionality, mock functionality, and utilities into
    separate files for filter group. Also adds a few more comments.

---
+++ blocks/filter-group/filter-group-utils.js
@@ -0,0 +1,23 @@
+/**
+ * @file Some helper utility functions for filter group.
+ */
+
+/**
+ * Update selected state of filter button.
+ * @param {HTMLButtonElement} buttonElement 
+ * @param {boolean} newSelected New selected state
+ */
+export const updateFilter = (buttonElement, newSelected = false) => {
+    buttonElement.classList.toggle('filter-group__button--selected', newSelected);
+    buttonElement.setAttribute('aria-pressed', newSelected ? 'true' : 'false');
+};
+
+/**
+ * Get an array containing the text value of all selected filters.
+ * When "All" is selected, this returns an empty array.
+ * @param {Element} parentElement 
+ * @returns {string[]}
+ */
+export const getCurrentFiltersArray = (parentElement) => Array.from(
+    parentElement.querySelectorAll('.filter-group__button:not(:first-of-type)[aria-pressed="true"]'), btn => btn.textContent
+) || [];
