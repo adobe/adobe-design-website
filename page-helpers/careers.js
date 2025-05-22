7f8b9c3c92da8b32e0dabdb2743e9b8e0cd9d7bb
Osmond Arnesto <osmond@heysparkbox.com>
2025-05-21 21:58:03 -0700

    feat: careers listing prefooter auto-block support

--- page-helpers/careers.js
+++ page-helpers/careers.js
@@ -1,13 +1,16 @@
 import { buildListingDetails } from "../blocks-helpers/listingDetails.js";
 import { buildListingCTA } from '../blocks-helpers/listingCTA.js';
+import { loadListingPreFooter } from '../blocks-helpers/listingPreFooter.js';
 
-export const buildCareersListingPage = () => {
+export const buildCareersListingPage = async () => {
   const listing = document.querySelector('#main-content > .section > div');
   listing.classList.add('util-listing-container');
   const listingHeader = listing.querySelector('h1');
   const listingDetails = buildListingDetails();
   const listingCTA = buildListingCTA();
+  const listingPreFooter = await loadListingPreFooter();
 
+  // Wrap main content of career listing in its own container for grid layout
   const listingMainContent = [...listing.children].filter(
     (child) => child !== listingHeader && child !==listingDetails
   );
@@ -17,19 +20,30 @@ export const buildCareersListingPage = () => {
     (child) => listingMainContentContainer.append(child)
   );
 
+  // Add horizontal rule between listing details and main content
   const horizontalRuleWrapper = document.createElement('div');
   horizontalRuleWrapper.classList.add('horizontal-rule-wrapper', 'util-hide-at-large');
   const horizontalRule = document.createElement('hr');
   horizontalRule.classList.add('horizontal-rule');
   horizontalRuleWrapper.append(horizontalRule);
 
+  // Add horizontal rule between listing and prefooter
+  const fullHorizontalRuleWrapper = document.createElement('div');
+  horizontalRuleWrapper.classList.add('horizontal-rule-wrapper', 'util-hide-at-large');
+  const fullHorizontalRule = document.createElement('hr');
+  fullHorizontalRule.classList.add('horizontal-rule', 'horizontal-rule--full');
+  fullHorizontalRuleWrapper.append(fullHorizontalRule);
+
+  listing.append(fullHorizontalRuleWrapper);
   listing.after(listingCTA);
+  listingCTA.after(listingPreFooter);
   listingHeader.after(listingDetails);
   listingDetails.after(horizontalRuleWrapper);
   horizontalRuleWrapper.after(listingMainContentContainer);
 
+  // Listing CTA only visible after scrolling past career details
   window.addEventListener('scroll', () => {
-    const revealListingCTAPosition = listingDetails.getBoundingClientRect().top;
+    const revealListingCTAPosition = listingDetails.getBoundingClientRect().bottom;
 
     if (revealListingCTAPosition < 0) {
       listingCTA.classList.remove('util-visually-hidden');
