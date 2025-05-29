7586568757fa7134b1a142a21676dbda28091177
Josh Winn <965114+jawinn@users.noreply.github.com>
2025-05-29 17:05:43 -0400

    docs: clear up docs on cleantext function

--- scripts/helpers/cleanText.js
+++ scripts/helpers/cleanText.js
@@ -1,16 +1,16 @@
 /**
- * Removes semantic element tags from a block of text.
+ * Takes the text from iterable elements and places it into span tags within an element.
+ * Useful for removing the semantic element tags from a block of text.
  * @function
- * @param {object} data - a set of HTML nodes from which to parse and replace tags
- * @param {object} element - the name of the parent element we are appending to
- * @return {string} an HTML Node set of spans
+ * @param {object} data - a set of HTML nodes from which to parse and replace tags.
+ * @param {object} element - the name of the parent element we are appending to.
  */
 
 export const cleanText = (data, element) => {
-    [...data].forEach((textNode) => {
-      const part = document.createElement('span');
-      part.textContent = textNode.textContent.trim();
-      element.append(part);
-    });
-  return
+  [...data].forEach((textNode) => {
+    const part = document.createElement('span');
+    part.textContent = textNode.textContent.trim();
+    element.append(part);
+  });
+  return;
 }
