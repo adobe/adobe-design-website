0cf0c1e6534772d215a361191f662c6b49be1ce6
Josh Winn <965114+jawinn@users.noreply.github.com>
2025-05-29 17:24:03 -0400

    feat: allow array of plain strings in appendcleantext
    Allow this function to also create the spans from an array of plain
    text, reducing the amount of code needed when recent stories uses
    the createcard function.

--- scripts/helpers/appendCleanText.js
+++ scripts/helpers/appendCleanText.js
@@ -1,16 +1,17 @@
 /**
- * Takes the text from iterable elements and places it into span tags within an element.
- * Useful for removing the semantic element tags from a block of text.
- * @function
- * @param {object} data - a set of HTML nodes from which to parse and replace tags.
- * @param {object} element - the name of the parent element we are appending to.
+ * Takes the text content from iterable elements and places it into span tags within a parent element.
+ * Useful for removing the semantic element tags (e.g. <h1>, <p>) from a block of text.
+ * 
+ * Alternatively, the text content can also be passed in as an array of plain strings to be placed into the spans.
+ *
+ * @param {HTMLCollection|string[]} data - A set of HTML nodes from which to parse and replace tags. Or, an array of text strings.
+ * @param {object} parentElement - The parent element to append the new span elements to.
  */
-
-export const appendCleanText = (data, element) => {
-  [...data].forEach((textNode) => {
+export const appendCleanText = (data, parentElement) => {
+  [...data].forEach((textItem) => {
     const part = document.createElement('span');
-    part.textContent = textNode.textContent.trim();
-    element.append(part);
+    part.textContent = typeof textItem === 'string' ? textItem.trim() : textItem.textContent.trim();
+    parentElement.append(part);
   });
   return;
 }
