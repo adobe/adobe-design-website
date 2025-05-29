95fbef4e18cfa7e1aa6bcb607b749eb0e87f0f21
Catherine Meade <cat@heysparkbox.com>
2025-05-28 21:59:35 -0400

    style: reorg helpers index for clarity

--- scripts/helpers/index.js
+++ scripts/helpers/index.js
@@ -1,12 +1,17 @@
+// helpers for building blocks
+export { getListingMetadata } from "./getListingMetadata.js";
+export { loadListingPreFooter } from "./loadListingPreFooter.js";
+export { buildListingCTA } from "./buildListingCTA.js";
+export { buildListingDetails } from "./buildListingDetails.js";
+export { buildSkipLink } from "./buildSkipLink.js";
+export { createFocusTrap } from "./createFocusTrap.js";
+
+// helpers for full-page logic
+export { buildCareersListingPage } from "./buildCareersListingPage.js";
+
+// general helpers
 export { cleanText } from "./cleanText.js";
 export { debounce } from "./debounce.js";
 export { isSubPageOf, isIndexPageOf } from "./isPageOf.js";
-export { createFocusTrap } from "./focusTrap.js";
-export { buildSkipLink } from "./skipLinks.js";
-export { getListingMetadata } from "./getListingMetadata.js";
-export { loadListingPreFooter } from "./listingPreFooter.js";
-export { buildListingCTA } from "./listingCTA.js";
-export { buildListingDetails } from "./listingDetails.js";
-export { buildCareersListingPage } from "./careers.js";
 export { prepURL } from "./prepURL.js";
 export { isExternalURL } from "./isExternalURL.js";
