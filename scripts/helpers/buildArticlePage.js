6c13e728f92fabbf026f70b4463ba6af076d0be5
Osmond Arnesto <osmond@heysparkbox.com>
2025-06-04 14:37:31 -0700

    fix: address feedback
      - JSDocs
      - try/catch blocks
      - article content in `<article>` element

--- scripts/helpers/buildArticlePage.js
+++ scripts/helpers/buildArticlePage.js
@@ -4,15 +4,30 @@ import {
   loadArticlePreFooter,
 } from "./index.js";
 
+/**
+ * Append an author bio and article prefooter with static content to
+ * article pages. Also moves main article content to a semantic article
+ * element.
+ *
+ * @return {void}
+ */
+
 export const buildArticlePage = async () => {
   const articleBodyContainer = document.querySelector(".article-header-container");
+  const articleElement = document.createElement("article");
+  
+  while (articleBodyContainer.firstChild) {
+    articleElement.appendChild(articleBodyContainer.firstChild);
+  };
 
   const authorData = await getAuthorData();
   authorData.forEach((author) => {
     const authorBio = buildAuthorBio(author);
-    articleBodyContainer.append(authorBio);
+    articleElement.append(authorBio);
   });
 
+  articleBodyContainer.append(articleElement);
+
   const articlePreFooter = await loadArticlePreFooter();
-  articleBodyContainer.append(articlePreFooter);
+  articleElement.after(articlePreFooter);
 }
--- scripts/helpers/buildAuthorBio.js
+++ scripts/helpers/buildAuthorBio.js
@@ -1,8 +1,26 @@
+/**
+ * Builds a block of information about an article's author on its page.
+ *
+ * @param {string} name
+ * @param {string} title
+ * @param {HTMLPictureElement} image
+ * @param {string} description
+ * @return {string} an HTML <div> node
+ */
+
 export const buildAuthorBio = ({ name, title, image, description }) => {
   const authorBio = document.createElement("div");
   authorBio.classList.add("author-bio");
+
+  const authorMeta = document.createElement("div");
+  authorMeta.classList.add("author-meta");
   const authorInfo = document.createElement("div");
   authorInfo.classList.add("author-meta__info");
+
+  if (image) {
+    image.classList.add("author-meta__image");
+    authorMeta.append(image);
+  };
 
   const authorName = document.createElement("span");
   authorName.classList.add("author-meta__name", "util-title-m");
@@ -16,18 +34,10 @@ export const buildAuthorBio = ({ name, title, image, description }) => {
     authorInfo.append(authorJobTitle);
   };
 
-  const authorMeta = document.createElement("div");
-  authorMeta.classList.add("author-meta");
-
-  if (image) {
-    image.classList.add("author-meta__image");
-    authorMeta.append(image);
-  };
-
   authorMeta.append(authorInfo);
 
   const authorDescription = document.createElement("div");
-  authorDescription.classList.add("author-description")
+  authorDescription.classList.add("author-description");
   description.forEach((part) => authorDescription.append(part));
 
   authorBio.append(authorMeta, authorDescription);
