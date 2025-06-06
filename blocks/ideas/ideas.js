921e2e3e8e6d97c06124103f6bb977781748d3e3
Josh Winn <965114+jawinn@users.noreply.github.com>
2025-06-06 11:34:24 -0400

    feat: ideas - working ideas loading with features
    Ideas loading and appearing mixed with features content.
    Work in progress load more button.

--- blocks/ideas/ideas.js
+++ blocks/ideas/ideas.js
@@ -1,3 +1,6 @@
+
+import { fetchAndBuildIdeas } from "../../scripts/helpers/fetchAndBuildIdeas.js";
+
 /**
  * Object representing each feature row in the content.
  * @typedef {object} IdeasFeatureContent
@@ -15,8 +18,6 @@
  * @returns {HTMLDivElement|null} Markup of feature, or null if missing required content.
  */
 const buildIdeasFeature = (featureContent) => {
-    console.warn(featureContent);
-    
     // Check for required content.
     if (!featureContent.headingText) {
         return null;
@@ -25,27 +26,27 @@ const buildIdeasFeature = (featureContent) => {
     // Create markup.
     // This element is both a grid item for the larger page layout, and a grid container for its children.
     const feature = document.createElement('div');
-    feature.classList.add('ideas-feature', 'grid-item--100');
+    feature.classList.add('ideas__feature', 'grid-item--100');
 
     // Content side which contains text elements and button.
     const mainContent = document.createElement('div');
-    mainContent.classList.add('ideas-feature__content');
+    mainContent.classList.add('ideas__feature-content');
 
     const heading = document.createElement('h2');
-    heading.classList.add('ideas-feature__heading');
+    heading.classList.add('ideas__feature-heading');
     heading.textContent = featureContent.headingText;
     mainContent.append(heading);
 
     if (featureContent.description && featureContent.description instanceof HTMLCollection) {
         const description = document.createElement('div');
-        description.classList.add('ideas-feature__description');
+        description.classList.add('ideas__feature-description');
         description.append(...featureContent.description);
         mainContent.append(description);
     }
 
     if (featureContent.buttonLink && featureContent.buttonLink instanceof HTMLAnchorElement) {
         const button = featureContent.buttonLink;
-        button.classList.add('ideas-feature__button', 'button', 'button--primary-outline');
+        button.classList.add('ideas__feature-button', 'button', 'button--primary-outline');
 
         if (featureContent.buttonAriaLabel) {
             button.setAttribute("aria-label", featureContent.buttonAriaLabel);
@@ -57,7 +58,7 @@ const buildIdeasFeature = (featureContent) => {
     let image = null;
     if (featureContent.image && featureContent.image instanceof HTMLPictureElement) {
         image = featureContent.image;
-        image.classList.add('ideas-feature__image');
+        image.classList.add('ideas__feature-image');
         image.setAttribute('alt', '');
         feature.append(image);
     }
@@ -65,6 +66,13 @@ const buildIdeasFeature = (featureContent) => {
     feature.append(mainContent);
     return feature;
 }
+
+const initialMaxIdeas = (totalFeatures, interval) => {
+    // At least the interval, if there are no features.
+    if (!totalFeatures) return interval;
+    // Set of cards above every feature and below the last feature.
+    return (totalFeatures * interval) + interval;
+};
 
 /*
  * Ideas Page Block
@@ -76,19 +84,21 @@ export default function decorate(block) {
     // Layout type setting.
     //const layout = block.children?.[1]?.children?.[0]?.textContent?.trim();
 
-    console.warn(block);
-
     // Features content, stored in row(s) after the first row.
     // There could be several features (recommended max of 4), or none.
     const featuresContent = Array.from(block.children ?? []).slice(1);
 
     // Create a new container to house the block.
     const newBlock = document.createElement('div');
-    newBlock.classList.add('ideas', 'grid-container');
+    newBlock.classList.add('ideas');
     newBlock.dataset.blockName = 'ideas';
 
-    // Add features to the block.
-    // Every 4th item at mobile. Every 5th item at medium. Every 9th item at extra-large.
+    const gridContainer = document.createElement('div');
+    gridContainer.classList.add('ideas__grid', 'grid-container');
+    newBlock.append(gridContainer);
+
+    // Create new features markup.
+    let features = [];
     featuresContent.forEach(feature => {
         /**
          * @type {IdeasFeatureContent}
@@ -97,19 +107,56 @@ export default function decorate(block) {
             headingText: feature?.children?.[0]?.textContent?.trim(),
             description: feature?.children?.[1]?.children,
             image: feature?.children?.[2]?.firstElementChild,
-            buttonLink: feature?.children?.[3]?.firstChild,
+            buttonLink: feature?.children?.[3]?.firstChild?.firstChild,
             buttonAriaLabel: feature?.children?.[4]?.textContent?.trim(),
         };
 
         const featureMarkup = buildIdeasFeature(featureContent);
         if (featureMarkup) {
-            newBlock.append(featureMarkup);
+            features.push(featureMarkup);
         }
     });
 
+    // How many cards before a feature, and how many additional to fetch on load more.
+    const interval = 4;
+
+    // Fetch and add markup for articles, and then add in the features.
+    const fetchAndAppend = async () => {
+        const fragment = await fetchAndBuildIdeas({
+            tagName: 'All',
+            maxArticles: initialMaxIdeas(features.length, interval),
+            gridItemClass: 'grid-item--25',
+            hasHorizontalScroll: false,
+        });
+
+        // Static node list of cards.
+        const cards = Array.from(fragment?.children ?? []);
+
+        // Add features in between cards in the grid:
+        // - Every 4th item at mobile.
+        // - Every 5th item at medium.
+        // - Every 9th item at extra-large.
+        features.forEach((feature, idx) => {
+            const cardIndexForInsert = (idx * interval) + interval - 1;
+            if (cardIndexForInsert > cards.length) {
+                // If more features than cards, stick features at the end.
+                fragment.append(feature);
+            } else {
+                cards[cardIndexForInsert].insertAdjacentElement('afterend', feature);
+            }
+        });
+
+        // Append load more button.
+        const loadMoreButton = document.createElement('button');
+        loadMoreButton.classList.add('ideas__load-button', 'button', 'button--primary');
+        loadMoreButton.textContent = 'Load more ideas';
+        newBlock.append(loadMoreButton);
+
+        // Append all cards and features.
+        gridContainer.append(fragment);
+    };
+    fetchAndAppend();
+
     // Replace the empty wrapper div around the block with our new markup.
     block.parentElement.replaceWith(newBlock);
-
-    // Fetch and add markup for articles (async).
-    //fetchDataAndBuildCards(newBlock, settings);
 }
