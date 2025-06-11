ebc8edf25464c9c7199c529ec33e4d3a67e37e52
Josh Winn <965114+jawinn@users.noreply.github.com>
2025-06-11 12:26:45 -0400

    fix: move theme backgrounds to helpers and remove repeated call

--- scripts/aem.js
+++ scripts/aem.js
@@ -11,7 +11,7 @@
  */
 
 /* eslint-env browser */
-import { decorateThemeBackgroundVisuals } from "./modules/themeBackgrounds.js";
+import { decorateThemeBackgroundVisuals } from "./helpers/themeBackgrounds.js";
 
 function sampleRUM(checkpoint, data) {
   // eslint-disable-next-line max-len
