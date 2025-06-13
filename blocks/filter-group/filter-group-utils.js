8c83663c88a2aa29658b0aff5b25b5e3b51f977b
Josh Winn <965114+jawinn@users.noreply.github.com>
2025-06-13 10:55:58 -0400

    refactor: add const for selector for clarity and run prettier

--- blocks/filter-group/filter-group-utils.js
+++ blocks/filter-group/filter-group-utils.js
@@ -4,23 +4,25 @@
 
 /**
  * Update selected state of filter button.
- * @param {HTMLButtonElement} buttonElement 
+ * @param {HTMLButtonElement} buttonElement
  * @param {boolean} newSelected New selected state
  */
 export const updateFilter = (buttonElement, newSelected = false) => {
-    buttonElement.classList.toggle('filter-group__button--selected', newSelected);
-    buttonElement.setAttribute('aria-pressed', newSelected ? 'true' : 'false');
+  buttonElement.classList.toggle("filter-group__button--selected", newSelected);
+  buttonElement.setAttribute("aria-pressed", newSelected ? "true" : "false");
 };
 
 /**
  * Get an array containing the text value of all selected filters.
- * When "All" is selected, this returns an empty array.
- * @param {Element} parentElement 
+ * When "All" is selected or the parent element does not exist, this returns an empty array.
+ * @param {Element} parentElement
  * @returns {string[]}
  */
 export const getCurrentFiltersArray = (parentElement) => {
-    if (!parentElement) return [];
-    return Array.from(
-        parentElement.querySelectorAll('.filter-group__button:not(:first-of-type)[aria-pressed="true"]'), btn => btn.textContent.trim()
-    ) || [];
-}
+  if (!parentElement) return [];
+  // Selected filter buttons other than the first button which is always "All".
+  const selectedFilters = parentElement.querySelectorAll(
+    '.filter-group__button:not(:first-of-type)[aria-pressed="true"]'
+  );
+  return Array.from(selectedFilters, (btn) => btn.textContent.trim()) || [];
+};
