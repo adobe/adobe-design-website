8cb616510b906c7b3d21387d886f8b1345e5bac4
Josh Winn <965114+jawinn@users.noreply.github.com>
2025-06-26 16:49:53 -0400

    feat: replace eyebrow tag with tags list buttons below bio
    Support multiple tags on articles using the new layout where they
    appear at buttons below the author bio. Removes eyebrow single tag
    link from top of article header.

--- scripts/helpers/buildArticlePage.js
+++ scripts/helpers/buildArticlePage.js
@@ -1,17 +1,51 @@
+import { getMetadata } from '../../scripts/aem.js';
 import {
   getAuthorData,
   buildAuthorAside,
   loadArticlePreFooter,
+  prepURL,
 } from "./index.js";
+
+/**
+ * Build list of linked article tags.
+ * @returns {HTMLDivElement|null}
+ */
+const buildArticleTags = () => {
+  const tags = getMetadata('tag');
+  if (!tags) return null;
+
+  const tagsWrapper = document.createElement("aside");
+  tagsWrapper.classList.add("tags-aside");
+
+  const heading = document.createElement("h2");
+  heading.classList.add("util-body-s");
+  heading.textContent = "Tags:";
+  tagsWrapper.append(heading);
+
+  const tagsList = document.createElement("ul");
+  tagsList.classList.add("tags-list");
+  tagsWrapper.append(tagsList);
+
+  const tagsArray = tags.split(',').map(item => item.trim());
+  tagsArray.forEach(tag => {
+    const tagListItem = document.createElement("li");
+    const link = document.createElement("a");
+    link.classList.add("button", "button--tag");
+    const urlSlug = prepURL(tag);
+    link.href = `/ideas/${urlSlug}`;
+    link.innerText = tag;
+    tagListItem.append(link);
+    tagsList.append(tagListItem);
+  });
+
+  return tagsWrapper;
+};
 
 /**
  * Append an author bio and article prefooter with static content to
  * article pages. Also moves main article content to a semantic article
  * element.
- *
- * @return {void}
  */
-
 export const buildArticlePage = async () => {
   if (window.errorCode === "404") return;
 
@@ -25,6 +59,9 @@ export const buildArticlePage = async () => {
   const authorData = await getAuthorData();
   const authorBios = buildAuthorAside(authorData);
   articleElement.append(authorBios);
+
+  const articleTags = buildArticleTags();
+  if (articleTags) articleElement.append(articleTags);
 
   articleBodyContainer.append(articleElement);
 
