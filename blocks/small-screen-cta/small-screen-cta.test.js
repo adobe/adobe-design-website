c6e458fa744b55b064eedf00dddc1b12eccd1d0b
Josh Winn <965114+jawinn@users.noreply.github.com>
2025-07-02 16:50:19 -0400

    fix: re-enable one skipped test after fix
    No longer skip one of the small screen CTA tests, after finding the
    issue with it. It was expecting null when the waitForSelector with
    the hidden: true option was actually returning the element.

--- blocks/small-screen-cta/small-screen-cta.test.js
+++ blocks/small-screen-cta/small-screen-cta.test.js
@@ -13,13 +13,17 @@ describe('Small Screen CTA Block', () => {
   describe("On large screens", () => {
     beforeAll(async () => {
       await page.setViewport({ width: 1500, height: 1000 });
-      await page.goto(`${global.BASE_URL}`);
-      await page.$('#main-content');
     });
 
-    xit("should not render the cta block", async () => {
+    it("should not render the cta block", async () => {
+      // Wait for element to not be found in the DOM or to be hidden.
+      // Note; this will still return the ElementHandle of the hidden element.
       const cta = await page.waitForSelector('.call-to-action', { hidden: true, timeout: 3000 });
-      expect(cta).toBeNull();
+      const isDisplayNone = await page.evaluate(el => {
+        const computedStyle = window.getComputedStyle(el);
+        return computedStyle.getPropertyValue('display') === 'none';
+      }, cta);
+      expect(isDisplayNone).toBe(true);
     });
   });
 });
