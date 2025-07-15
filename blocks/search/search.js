2e49e891f8b3c37736cea69226b0209bd41ba7f8
Josh Winn <965114+jawinn@users.noreply.github.com>
2025-07-15 11:55:42 -0400

    feat: allow search to find articles by author names
    Within the site search, include the author name in the text that is
    searched, for articles only.

--- blocks/search/search.js
+++ blocks/search/search.js
@@ -4,6 +4,10 @@ const searchParams = new URLSearchParams(window.location.search);
 const SEARCH_INPUT_ICON = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" focusable="false" aria-hidden="true" roll="img"><path fill="currentColor" d="M16.9 15.5c2.4-3.2 2.2-7.7-.7-10.6-3.1-3.1-8.1-3.1-11.3 0-3.1 3.2-3.1 8.3 0 11.4 2.9 2.9 7.5 3.1 10.6.6v.1l4.2 4.2c.5.4 1.1.4 1.5 0 .4-.4.4-1 0-1.4l-4.3-4.3zm-2.1-9.2c2.3 2.3 2.3 6.1 0 8.5-2.3 2.3-6.1 2.3-8.5 0C4 12.5 4 8.7 6.3 6.3c2.4-2.3 6.2-2.3 8.5 0z"/></svg>`;
 const SEARCH_CLEAR_ICON = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8" focusable="false" aria-hidden="true" role="img"><path fill="currentColor" d="m5.238 4 2.456-2.457A.875.875 0 1 0 6.456.306L4 2.763 1.543.306A.875.875 0 0 0 .306 1.544L2.763 4 .306 6.457a.875.875 0 1 0 1.238 1.237L4 5.237l2.456 2.457a.875.875 0 1 0 1.238-1.237z"></path></svg>`;
 
+// Root directory names for main sections (without slashes).
+const articleRoot = "ideas";
+const authorRoot = "authors";
+const jobListingRoot = "careers";
 
 /**
  * Create section name to be used for descriptive text under search result title.
@@ -23,10 +27,10 @@ function sectionNameFromPath(path) {
     sectionName = rootDir;
 
     // Custom sub-page name adjustments.
-    if (sectionName === "ideas") {
+    if (sectionName === articleRoot) {
       sectionName = "Article";
     }
-    if (sectionName === "careers") {
+    if (sectionName === jobListingRoot) {
       sectionName = "Job Listing";
     }
   }
@@ -126,12 +130,12 @@ function filterData(searchTerms, data) {
     let minIdx = -1;
 
     // Leave home page and author pages off of results.
-    if (result?.path === '/' || result?.path.startsWith('/authors/')) return;
+    if (result?.path === '/' || result?.path.startsWith("/" + authorRoot + "/")) return;
 
-    // Search within meta `title`, `description`, and the words in the last part of the `path`.
-    const metaContents = `${result.title} ${!result.path.startsWith('/authors/') ? result.description : ''} ${result.path.split('/').pop()}`.toLowerCase();
+    // Search within meta `title`, `description`, the words in the last part of the `path`, and the author name on articles.
+    const textToSearch = `${result.title} ${result.description} ${result.path.split('/').pop()} ${result.path.startsWith("/" + articleRoot + "/") ? result.author : ''}`.toLowerCase();
     searchTerms.forEach((term) => {
-      const idx = metaContents.indexOf(term);
+      const idx = textToSearch.indexOf(term);
       if (idx < 0) return;
       if (minIdx < idx) minIdx = idx;
     });
