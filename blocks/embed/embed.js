e670d2f7b997f65e949e023309e62d7cc17791ba
Osmond Arnesto <osmond@heysparkbox.com>
2025-07-18 12:19:52 -0700

    feat: import old video block and rename to embed

---
+++ blocks/embed/embed.js
@@ -0,0 +1,27 @@
+export default async function decorate(block) {
+  // Get embed data
+  const embed = {
+    code: block.children?.[0]?.children?.[0]?.children?.[0]?.innerText,
+    caption: block.children?.[0]?.children?.[0]?.children?.[1]?.innerText,
+  };
+
+  // Create a figure and populate it with the embed code
+  const figure = document.createElement("figure");
+  figure.classList.add("embed");
+  figure.innerHTML = embed.code;
+
+  // Apply classes to the iframe
+  const iframe = figure.querySelector("iframe");
+  iframe.classList.add("embed__iframe");
+  if (embed.code.includes("spotify")) iframe.classList.add("embed__iframe--no-aspect-ratio");
+
+  // Create a figcaption, if it exists, and append to the figure
+  if (embed.caption) {
+    const figCaption = document.createElement("figcaption");
+    figCaption.className = "embed__caption";
+    figCaption.innerHTML = embed.caption;
+    figure.appendChild(figCaption);
+  }
+
+  block.replaceWith(figure);
+}
